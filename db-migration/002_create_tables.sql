9b75df628b993a4f6ba485a28794076371ed86b8
Isma-corudo <ismael.lernanto@gmail.com>
2024-03-07 00:16:28 -0300

    modificações -> traduzido campos, corriguito primary key composta, alterado a ordens da criação dos enum ticket_type, ticket_class.

--- db-migration/002_create_tables.sql
+++ db-migration/002_create_tables.sql
@@ -1,16 +1,23 @@
 begin;
+
+-- Criar o tipo ENUM ticket_type
+CREATE TYPE ticket_type AS ENUM ('standard', 'vip', 'premium');
+
+-- Criar o tipo ENUM ticket_class
+CREATE TYPE ticket_class AS ENUM ('standard', 'senior', 'student', 'promotional');
+
 ---------------------
 -- Create tables
 ---------------------
 
 -- Create USER table
-create table IF NOT EXISTS public.tb_empresa (
+.create table IF NOT EXISTS public.tb_empresas (
   id              serial       not null,
   name            varchar(255) not null,
   cnpj            varchar(14)  not null         primary key,
   email           varchar(255) not null,
-  created_at      timestamptz  not null         default current_timestamp,
-  updated_at      timestamptz  not null         default current_timestamp
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
 
 
@@ -18,23 +25,23 @@ create table IF NOT EXISTS public.tb_empresa (
 create table IF NOT EXISTS public.tb_eventos (
   id              bigint        not null        primary key,
   id_empresa      BIGINT        NOT NULL        REFERENCES tb_empresas(id),
-  title           varchar(255)  not null,
-  description     text          not null,
-  value           float         not null,
-  created_at      timestamptz   not null        default current_timestamp,
-  updated_at      timestamptz   not null        default current_timestamp
+  titulo          varchar(255)  not null,
+  descrição       text          not null,---verificar se o 'Ç' não quebra nada
+  valor           float         not null,
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
 
 
 -- Create INGRESSOS table
 create table IF NOT EXISTS public.tb_ingressos (
-  id              bigint        not null        primary key,
-  type            ticket_type   not null,
-  value           float         not null,
-  id_evento       bigint        not null        REFERENCES tb_eventos(id),
-  id_usuario      bigint        not null        REFERENCES tb_usuarios(id),
-  created_at      timestamptz   not null        default current_timestamp,
-  updated_at      timestamptz   not null        default current_timestamp
+  id               bigint        not null        primary key,
+  tipo             ticket_type   not null,
+  valor            float         not null,
+  id_eventos       bigint        not null        REFERENCES tb_eventos(id),
+  id_usuarios      bigint        not null        REFERENCES tb_usuarios(id),
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
 
 
@@ -44,11 +51,12 @@ create table IF NOT EXISTS public.tb_usuarios (
   name            varchar(255) not null,
   cpf             varchar(11)  not null,
   email           varchar(255) not null         primary key,
-  password        varchar(255) not null,
+  senha           varchar(255) not null,
   token           varchar(255) not null,
-  created_at      timestamptz  not null         default current_timestamp,
-  updated_at      timestamptz  not null         default current_timestamp
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
+
 
 -- Create CARTÃO table
 create table IF NOT EXISTS public.tb_dados_cartao (
@@ -58,42 +66,36 @@ create table IF NOT EXISTS public.tb_dados_cartao (
   numero_cartao           varchar(255) not null,
   data_expiracao          date         not null,
   codigo_seguranca        varchar(4)   not null,
-  id_usuario              bigint       not null     REFERENCES tb_usuarios(id),
-  created_at              timestamptz  not null     default current_timestamp,
-  updated_at              timestamptz  not null     default current_timestamp,
+  id_usuarios             bigint       not null     REFERENCES tb_usuarios(id),
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 
   -- Definir a chave primária composta
-  CONSTRAINT pk_dados_cartao PRIMARY KEY (numero_cartao, data_expiracao, codigo_seguranca)
+  PRIMARY KEY (numero_cartao, data_expiracao, codigo_seguranca)
 );
 
 
-
--- Create PAGAMENTO table
+-- Create PAGAMENTOS table
 create table IF NOT EXISTS public.tb_pagamentos (
   id                      bigint        not null    primary key,
   quantidade_ingressos    int           not null,
   valo_final              float         not null,
-  id_usuario              bigint        not null    REFERENCES tb_usuarios(id),
-  id_ingresso             bigint        not null    REFERENCES tb_ingresso(id),
-  created_at              timestamptz   not null    default current_timestamp,
-  updated_at              timestamptz   not null    default current_timestamp
+  id_usuarios              bigint        not null    REFERENCES tb_usuarios(id),
+  id_ingressos             bigint        not null    REFERENCES tb_ingressos(id),
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
+
 
 -- Create CARRINHO table
 create table IF NOT EXISTS public.tb_carrinho (
-  id_usuario              bigint        not null    REFERENCES tb_usuarios(id),
-  id_ingresso             bigint        not null    REFERENCES tb_ingresso(id),
-  class                   ticket_class  not null,
-  valo_final              float         not null,
-  created_at              timestamptz   not null    default current_timestamp,
-  updated_at              timestamptz   not null    default current_timestamp
+  id_usuarios              bigint        not null    REFERENCES tb_usuarios(id),
+  id_ingressos             bigint        not null    REFERENCES tb_ingressos(id),
+  classe                   ticket_class  not null,
+  desconto                 float         not null,
+  criado_data             timestamptz  not null     default current_timestamp,
+  modificado_data         timestamptz  not null     default current_timestamp,
 );
 
-
--- Criar o tipo ENUM ticket_type
-CREATE TYPE ticket_type AS ENUM ('standard', 'vip', 'premium');
-
--- Criar o tipo ENUM ticket_class
-CREATE TYPE ticket_class AS ENUM ('standard', 'senior', 'student', 'promotional');
 
 commit;
