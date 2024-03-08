bb42d09775b91253fe665a914dcc0f517e6daf4b
Luiz Fernando <luizfvidal3@hotmail.com>
2024-03-08 05:31:09 -0300

    adicionado dados dos pagamentos

---
+++ db-migration/105_pagamentos.seed.sql
@@ -0,0 +1,11 @@
+insert into public.tb_pagamentos
+    ("id", "quantidade_ingressos", "valor_final", "id_usuario", "id_ingresso") 
+values 
+(1, 2, 170.00, 1, 1),
+(2, 3, 210.00, 1, 4),
+(3, 1, 55.00,  2, 8),
+(4, 2, 240.00, 2, 6),
+(5, 3, 600.00, 1, 18),
+(6, 4, 440.00, 2, 15),
+(7, 1, 25.00,  2, 10),
+(8, 2, 300.00, 1, 5);
