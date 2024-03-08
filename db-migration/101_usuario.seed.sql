f2af8cad0bfb95d1cf36f141f75e248a8cecf560
Bruno Natali <bruno.natali@quality24.com.br>
2024-03-08 00:37:47 -0300

    chore: Corrects user hashed passwords to '0000'
    Refs: #1

--- db-migration/101_usuario.seed.sql
+++ db-migration/101_usuario.seed.sql
@@ -1,5 +1,5 @@
 insert into public.tb_usuarios 
   ("id", "nome", "cpf", "email", "senha") 
 values
-  (1, 'Laura Smith', '98765432100','laurasmith@email.com', '$2b$10$JJXbrLroa/5r4OQZ9McFKO5JrKjUa71tAWobZGxVVQ.45cXbZlvF2'),
-  (2, 'David Johnson', '12345678900','davidjohnson@email.com', '$2b$10$JJXbrLroa/5r4OQZ9McFKO5JrKjUa71tAWobZGxVVQ.45cXbZlvF2');
+  (1, 'Laura Smith', '98765432100','laurasmith@email.com', '$2b$10$MZiaXPO2X1Y/q3Ql.8JSleUlW9itbFIODoaRc3Qjtr3HLKIaCgwPK'),
+  (2, 'David Johnson', '12345678900','davidjohnson@email.com', '$2b$10$MZiaXPO2X1Y/q3Ql.8JSleUlW9itbFIODoaRc3Qjtr3HLKIaCgwPK');
