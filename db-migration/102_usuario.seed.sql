09866b2b9793d17d412fb80a4bbf5d06049069f6
Luiz Fernando <luizfvidal3@hotmail.com>
2024-03-07 22:26:04 -0300

    insert dos dados de 6  eventos e de 2 usuarios

---
+++ db-migration/102_usuario.seed.sql
@@ -0,0 +1,3 @@
+insert into public.tb_usuarios ("id", "name", "cpf", "email", "senha", "token") values
+(1, 'Laura Smith', '98765432100','laurasmith@email.com', '$2b$10$JJXbrLroa/5r4OQZ9McFKO5JrKjUa71tAWobZGxVVQ.45cXbZlvF2', '7f92e4a1d3b5c6f8'),
+(2, 'David Johnson', '12345678900','davidjohnson@email.com', '$2b$10$JJXbrLroa/5r4OQZ9McFKO5JrKjUa71tAWobZGxVVQ.45cXbZlvF2', '9b3c8d7a2e6f4c1');
