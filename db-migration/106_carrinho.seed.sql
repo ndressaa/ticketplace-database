170b36744a4b39fe9238609fdecf13b522e7e1f1
Luiz Fernando <luizfvidal3@hotmail.com>
2024-03-08 05:51:40 -0300

    adicionado dados do carrinho de compras

---
+++ db-migration/106_carrinho.seed.sql
@@ -0,0 +1,12 @@
+
+insert into public.tb_carrinho
+    ("id_usuario", "id_ingresso", "classe", "desconto")
+values
+(1, 1, 'senior', 00.00),
+(1, 4, 'student', 105.00 ),
+(2, 8, 'promotional', 20.00),
+(2, 6, 'senior', 00.00),
+(1, 18, 'student', 300.00),
+(2, 15, 'promotional', 80.00),
+(2, 10, 'senior', 00.00),
+(1, 5, 'student', 150.00);
