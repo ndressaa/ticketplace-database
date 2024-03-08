eb2f933017242d1c600dfe034fb9ca8e61485431
Luiz Fernando <luizfvidal3@hotmail.com>
2024-03-08 05:58:10 -0300

    alterado alguns erros no insert da carrinho

--- db-migration/106_carrinho.seed.sql
+++ db-migration/106_carrinho.seed.sql
@@ -3,10 +3,10 @@ insert into public.tb_carrinho
     ("id_usuario", "id_ingresso", "classe", "desconto")
 values
 (1, 1, 'senior', 00.00),
-(1, 4, 'student', 105.00 ),
-(2, 8, 'promotional', 20.00),
+(1, 4, 'estudante', 105.00 ),
+(2, 8, 'promocional', 20.00),
 (2, 6, 'senior', 00.00),
-(1, 18, 'student', 300.00),
-(2, 15, 'promotional', 80.00),
+(1, 18, 'estudante', 300.00),
+(2, 15, 'promocional', 80.00),
 (2, 10, 'senior', 00.00),
-(1, 5, 'student', 150.00);
+(1, 5, 'estudante', 150.00);
