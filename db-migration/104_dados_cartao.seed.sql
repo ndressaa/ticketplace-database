6a218983f6714ea70e648493254b069ec2858622
Luiz Fernando <luizfvidal3@hotmail.com>
2024-03-08 05:19:21 -0300

    adicionado dados dos cartões de crédito

---
+++ db-migration/104_dados_cartao.seed.sql
@@ -0,0 +1,5 @@
+insert into public.tb_dados_cartao
+     ("id", "nome_cartao", "cpf_titular", "numero_cartao", "data_expiracao", "codigo_seguranca", "id_usuario") 
+values
+(1, 'Laura Smith', '98765432100', '4532879012345678', '2025-12-01', '321', 1),
+(2, 'David Johnson', '12345678900', '5410 6543 9876 1234', '2025-09-23', '654', 2 );
