75399648dd01d1029c82334d75267478079e1c72
Bruno Natali <b010010010n@gmail.com>
2024-03-09 14:41:48 -0300

    Isma sand box (#5)
    * tabela_inicial
    
    * alteração no schema.db e criação de tabelas (não testado no bamco de dados)
    
    * alteração no nome do arquivo 002_Create_tables.sql para 002_main.schema.sql
    
    * adicionado campo categoria na tabela eventos Refs: #1
    
    * relação entre tabelas e pequenas correções
    
    * modificações -> traduzido campos, corriguito primary key composta, alterado a ordens da criação dos enum ticket_type, ticket_class.
    
    * acertar pontuação
    
    * adicionando .schama no nome do arquivo
    
    * adicionado campos tb eventos
    
    * insert dos dados da empresa
    
    * insert dos dados de 6  eventos e de 2 usuarios
    
    * constraint de relação entre a tabelas
    
    * chore: Corrected schema & added events photos
    
    Refs: #1
    
    * chore: Corrects column `id_usuario`'s name
    
    Refs: #1
    
    * chore: Corrects `id_ingresso`'s column name
    
    Refs: #1
    
    * chore: Corrects user hashed passwords to '0000'
    
    Refs: #1
    
    * adicionado dados dos ingressos
    
    * adicionado dados dos cartões de crédito
    
    * adicionado dados dos pagamentos
    
    * adicionado dados do carrinho de compras
    
    * alterado alguns erros no insert da carrinho
    
    ---------
    
    Co-authored-by: Isma-corudo <ismael.lernanto@gmail.com>
    Co-authored-by: Luiz Fernando <luizfvidal3@hotmail.com>
    Co-authored-by: Bruno Natali <bruno.natali@quality24.com.br>

---
+++ db-migration/106_carrinho.seed.sql
@@ -0,0 +1,12 @@
+
+insert into public.tb_carrinho
+    ("id_usuario", "id_ingresso", "classe", "desconto")
+values
+(1, 1, 'senior', 00.00),
+(1, 4, 'estudante', 105.00 ),
+(2, 8, 'promocional', 20.00),
+(2, 6, 'senior', 00.00),
+(1, 18, 'estudante', 300.00),
+(2, 15, 'promocional', 80.00),
+(2, 10, 'senior', 00.00),
+(1, 5, 'estudante', 150.00);
