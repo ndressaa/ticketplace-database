5975d996c517f2bc0c0fcddfd08902331dda24ad
Bruno Natali <b010010010n@gmail.com>
2024-09-09 20:09:37 -0300

    Feature/exchange (#11)
    * chore: fix docker configs & trigger names
    
    * Isma sand box (#5)
    
    * tabela_inicial
    
    * alteração no schema.db e criação de tabelas (não testado no bamco de dados)
    
    * alteração no nome do arquivo 002_Create_tables.sql para 002_main.schema.sql
    
    * adicionado campo categoria na tabela eventos Refs: #1
    
    * relação entre tabelas e pequenas correções
    
    * modificações -> traduzido campos, corriguito primary key composta, alterado a ordens da criação dos enum ticket_type, ticket_class.
    
    * acertar pontuação
    
    * adicionando .schama no nome do arquivo
    
    * adicionado campos tb eventos
    
    * insert dos dados da empresa
    
    * insert dos dados de 6  eventos e de 2 usuarios
    
    * constraint de relação entre a tabelas
    
    * chore: Corrected schema & added events photos
    
    Refs: #1
    
    * chore: Corrects column `id_usuario`'s name
    
    Refs: #1
    
    * chore: Corrects `id_ingresso`'s column name
    
    Refs: #1
    
    * chore: Corrects user hashed passwords to '0000'
    
    Refs: #1
    
    * adicionado dados dos ingressos
    
    * adicionado dados dos cartões de crédito
    
    * adicionado dados dos pagamentos
    
    * adicionado dados do carrinho de compras
    
    * alterado alguns erros no insert da carrinho
    
    ---------
    
    Co-authored-by: Isma-corudo <ismael.lernanto@gmail.com>
    Co-authored-by: Luiz Fernando <luizfvidal3@hotmail.com>
    Co-authored-by: Bruno Natali <bruno.natali@quality24.com.br>
    
    * chore: adds group members to README
    
    Refs: #1
    
    * chore: correctd readme format
    
    Refs: #1
    
    * feat: Add address table & implements support tables to exchange
    
    Refs: #3 #8 #9 #10
    
    * chore: Add triggers to new tables, to auto handle changes
    
    Refs: Criar tabela ingressos comprados #10
    Criar tabela para guardar ingressos gerados #9
    Criar tabela de troca de ingressos #8
    
    * chore: Corrects refferences
    
    ---------
    
    Co-authored-by: Bruno Natali <bruno.natali@quality24.com.br>
    Co-authored-by: Isma-corudo <ismael.lernanto@gmail.com>
    Co-authored-by: Luiz Fernando <luizfvidal3@hotmail.com>

---
+++ db-migration/003_exchange_support.schema.sql
@@ -0,0 +1,168 @@
+begin;
+
+---
+-- Cria tabela de endereços e adiciona coluna de endereço nas tabelas de empresas, eventos e usuários
+--
+-- #3
+---
+
+create table tb_enderecos (
+  id bigserial primary key,
+  cep varchar(8) not null,
+  rua varchar(255) not null,
+  numero varchar(10) not null,
+  complemento varchar(255),
+  bairro varchar(255) not null,
+  cidade varchar(255) not null,
+  estado varchar(2) not null,
+  criado_data timestamptz not null default current_timestamp,
+  atualizado_data timestamptz not null default current_timestamp
+);
+
+-- Cria um trigger na tabela `tb_enderecos` para gerenciar automaticamente as colunas de `timestamp`
+create trigger trigger_gerencia_data_tb_enderecos
+  before insert or update on tb_enderecos
+  for each row execute procedure gerencia_coluna_timestamp();
+
+alter table tb_empresas add column id_endereco bigint references tb_enderecos(id);
+
+alter table tb_eventos add column id_endereco bigint references tb_enderecos(id);
+
+alter table tb_usuarios add column id_endereco bigint references tb_enderecos(id);
+
+-- Cria um tipo para definir o tipo de ingresso e adiciona uma coluna na tabela carrinho de compras
+
+create type tipo_ingresso as enum ('normal', 'venda', 'troca', 'devolucao');
+
+alter table tb_carrinho add column tipo_ingresso tipo_ingresso not null;
+alter table tb_carrinho alter column tipo_ingresso set default 'normal';
+
+
+---
+-- Cria tabela de ingressos comprados
+--
+-- #10
+---
+
+create table tb_ingressos_comprados (
+  id bigserial primary key,
+  id_usuario bigint not null references tb_usuarios(id),
+  id_ingresso bigint not null references tb_ingressos(id),
+  -- id_ingresso_gerado bigint references tb_ingressos_gerados(id),
+  -- id_ingresso_devolvido bigint references tb_ingresso_devolvido(id),
+  criado_data timestamptz not null default current_timestamp,
+  atualizado_data timestamptz not null default current_timestamp
+);
+
+-- Cria um trigger na tabela `tb_ingressos_comprados` para gerenciar automaticamente as colunas de `timestamp`
+create trigger trigger_gerencia_data_tb_ingressos_comprados
+  before insert or update on tb_ingressos_comprados
+  for each row execute procedure gerencia_coluna_timestamp();
+
+
+---
+-- Cria tabela de ingressos gerados
+--
+-- #9
+---
+
+create table tb_ingressos_gerados (
+  id bigserial primary key,
+  id_ingresso_comprado bigint not null references tb_ingressos_comprados(id),
+  valor_hash text not null,
+  imagem_ingresso text,
+  criado_data timestamptz not null default current_timestamp,
+  atualizado_data timestamptz not null default current_timestamp
+);
+
+alter table tb_ingressos_comprados add column id_ingresso_gerado bigint references tb_ingressos_gerados(id);
+
+-- Cria um trigger na tabela `tb_ingressos_gerados` para gerenciar automaticamente as colunas de `timestamp`
+create trigger trigger_gerencia_data_tb_ingressos_gerados
+  before insert or update on tb_ingressos_gerados
+  for each row execute procedure gerencia_coluna_timestamp();
+
+-- Cria uma função para inserir o id do ingresso gerado na coluna `id_ingresso_gerado` da tabela de ingressos comprados
+create or replace function insere_id_ingresso_gerado()
+  returns trigger as $$
+  begin
+    update public.tb_ingressos_comprados
+    set id_ingresso_gerado = new.id
+    where id = new.id_ingresso_comprado;
+
+    return new;
+  end;
+$$ language plpgsql volatile;
+
+-- Cria um triger na tabela `tb_ingressos_gerados` para inserir o id do ingresso gerado na coluna `id_ingresso_gerado` da tabela de ingressos comprados
+create trigger trigger_insere_id_ingresso_gerado
+  after insert on tb_ingressos_gerados
+  for each row
+  execute procedure insere_id_ingresso_gerado();
+
+---
+-- Cria tabela para fluxo de devolução de ingressos
+--
+-- #8
+---
+
+create type tipo_devolucao_ingresso as enum ('venda', 'troca', 'devolucao');
+
+create table tb_ingresso_devolvido (
+  id bigserial primary key,
+  id_ingresso_comprado bigint not null references tb_ingressos_comprados(id),
+  tipo_devolucao tipo_devolucao_ingresso not null,
+  criado_data timestamptz not null default current_timestamp,
+  atualizado_data timestamptz not null default current_timestamp
+);
+
+alter table tb_ingressos_comprados add column id_ingresso_devolvido bigint references tb_ingresso_devolvido(id);
+
+-- Cria um trigger na tabela `tb_ingresso_devolvido` para gerenciar automaticamente as colunas de `timestamp`
+create trigger trigger_gerencia_data_tb_ingresso_devolvido
+  before insert or update on tb_ingresso_devolvido
+  for each row execute procedure gerencia_coluna_timestamp();
+
+-- Cria uma função para inserir o id do ingresso devolvido na coluna `id_ingresso_devolvido` da tabela de ingressos comprados
+create or replace function insere_id_ingresso_devolvido()
+  returns trigger as $$
+  begin
+    update public.tb_ingressos_comprados
+    set id_ingresso_devolvido = new.id
+    where id = new.id_ingresso_comprado;
+
+    return new;
+  end;
+$$ language plpgsql volatile;
+
+-- Cria um triger na tabela `tb_ingresso_devolvido` para inserir o id do ingresso devolvido na coluna `id_ingresso_devolvido` da tabela de ingressos comprados
+create trigger trigger_insere_id_ingresso_devolvido
+  after insert on tb_ingresso_devolvido
+  for each row
+  execute procedure insere_id_ingresso_devolvido();
+
+-- Cria uma função para garantir que um ingresso não seja devolvido mais de uma vez
+create or replace function unico_ingresso_devolvido()
+  returns trigger as $$
+  begin
+    if exists (
+      select 1
+      from tb_ingresso_devolvido
+      where id_ingresso_comprado = new.id_ingresso_comprado
+    ) then
+      raise exception 'Ingresso já foi devolvido';
+    end if;
+
+    return new;
+  end;
+$$ language plpgsql volatile;
+
+-- Cria um triger na tabela `tb_ingresso_devolvido` para garantir que um ingresso não seja devolvido mais de uma vez
+create trigger trigger_unico_ingresso_devolvido
+  before insert on tb_ingresso_devolvido
+  for each row
+  execute procedure unico_ingresso_devolvido();
+
+
+
+commit;
---
+++ db-migration/109_enderco.seed.sql
@@ -0,0 +1,15 @@
+
+insert into public.tb_enderecos 
+  ("id", "cep", "rua", "numero", "complemento", "bairro", "cidade", "estado")
+values
+  (1, '12345678', 'Rua Sete de Setembro', '10', 'Apto 101', 'Centro', 'São Paulo', 'SP'),
+  (2, '87654321', 'Av. Nove de Julho', '100', 'Apto 202', 'Jardim', 'Rio de Janeiro', 'RJ'),
+  (3, '12345678', 'Av. Marquês', '1000', 'Apto 303', 'Centro', 'São Paulo', 'SP'),
+  (4, '87654321', 'Rua dos Estados', '100', 'Sala 202', 'Jardim', 'Rio de Janeiro', 'RJ'),
+  (5, '12345678', 'Rua dos Andradas', '1000', 'Sala 303', 'Centro', 'São Paulo', 'SP'),
+  (6, '87654321', 'Av. Paulista', '100', null, 'Jardim', 'Rio de Janeiro', 'RJ'),
+  (7, '12345678', 'Rua Augusta', '1000', null, 'Centro', 'São Paulo', 'SP'),
+  (8, '87654321', 'Av. Brasil', '100', null, 'Jardim', 'Rio de Janeiro', 'RJ'),
+  (9, '12345678', 'Rua dos Pinheiros', '1000', null, 'Centro', 'São Paulo', 'SP'),
+  (10, '87654321', 'Av. Paulista', '100', null, 'Jardim', 'Rio de Janeiro', 'RJ'),
+  (11, '12345678', 'Rua Jandira', '1000', null, 'Centro', 'São Paulo', 'SP');
